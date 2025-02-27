2d90ccd07b57e5d3b2e9ad67ddf6c457615e78bd
xiangnanyue <xiangnanyue@users.noreply.github.com>
2025-02-27 21:08:58 +0800

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -27,8 +27,8 @@ pub struct ExchangeInfo {
     pub rate_limits: Option<Vec<RateLimit>>,
     pub server_time: Option<i64>,
     pub assets: Option<Vec<Asset>>,
-    pub symbols: Option<Vec<Symbol>>,
-    pub timezone: Option<String>,
+    pub symbols: Vec<Symbol>,
+    pub timezone: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
